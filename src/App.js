03c8741ea549dcc0cc8e6f62df2d567a10107ca5
Chinmay Bhole <chinmaymotog5@gmail.com>
2023-02-21 21:17:21 +0530

    added a logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import TodoForm from "./components/TodoForm";
 import Todos from "./components/Todos";
+import logo from "../src/logo-color.png";
 
 function App() {
   return (
     <div>
+      <div class="loader">
+        <img src={logo} alt="logo" />
+      </div>
       <Header />
       <TodoForm />
       <Todos />
